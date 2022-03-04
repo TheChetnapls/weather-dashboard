ba1fd69723e21d4912f45de17606b73fa70df49e
TheChetnapls <36456949+TheChetnapls@users.noreply.github.com>
2022-03-03 18:22:29 -0800

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+// Global variables
+var searchHistory = [];
+var weatherApiRootUrl = 'https://api.openweathermap.org';
+var weatherApiKey = '2dc18646a341fb4a90e14ab98eb507c6';
+
+// Grab DOM elements
